dd97425687852c5a748bc6a4ebabf2a5bb2506ea
Mateo <700384+IAmMateo@users.noreply.github.com>
2018-07-29 17:48:10 -0400

    Update header.js
    Added header/headline info

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Matthew Hill</h1>
+                <p>A certified, experienced Quality Assurance Engineer and Tester <br />
+                seeking new opportunities for both manual and automation testing.</p>
             </div>
         </div>
         <nav>
